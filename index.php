e740925bdfa416d36d0514039ce5c76f76e97544
david salas <95159629+cuervoc@users.noreply.github.com>
2024-08-15 23:35:03 -0400

    lol21

--- index.php
+++ index.php
@@ -8,8 +8,8 @@
 <body>
 <?php
 $api_key = 'RGAPI-e45bf63a-f663-4ca2-8e1b-0f3103772db7';
-$summoner_name = 'nombre_del_invocador';
-$region = 'euw1'; // Cambia esto a la región adecuada
+$summoner_name = 'msfcuervoc';
+$region = 'la2'; // Cambia esto a la región adecuada
 
 $url = "https://$region.api.riotgames.com/lol/summoner/v4/summoners/by-name/$summoner_name?api_key=$api_key";
 
